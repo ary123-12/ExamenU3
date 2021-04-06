4c462878370d2a8d429dadf05575a6ca72d1f970
ary123-12 <arianti.sac28@gmail.com>
2021-04-06 10:11:48 -0500

    Ariel Antonio Sanchez 2.0

--- examenu3/examenu3/settings.py
+++ examenu3/examenu3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ariel',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'examenu3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ariel/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
